550a02a33c22d2b7b4e5bca7b3329875174f101f
Rayssa Emelly Moura da Silva <rayssaemelly20@gmail.com>
2026-02-13 11:06:56 -0300

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,5 @@
+
+raio = float(input("digite o valor do raio do cilindro: "))
+altura = float(input("digite a altura do cilindro: "))
+valor = 3.14 * (raio*raio) * altura
+print(f"o valor do volume do cilindro é aproximadamente: {valor}")
