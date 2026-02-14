1c0bb57da2594c00482cfaa22f73ad4431b2e8cc
Rayssa Emelly Moura da Silva <rayssaemelly20@gmail.com>
2026-02-14 12:28:43 -0300

    Update q8.py

--- q8.py
+++ q8.py
@@ -1,20 +1,22 @@
 #Questão 08
- 
- medida = input("Qual a unidade de medidas que quer converter?(metros,centimetros,milhas) ")
- valor = float(input("Digite o valor: "))
- convert = input("Para o qual quer converter? (metros,centimentros ou milhas)")
- if medida == "milhas" and convert == "metros":
+medida = input("Qual a unidade de medidas que quer converter?(metros,centimetros,milhas) ")
+valor = float(input("Digite o valor: "))
+convert = input("Para o qual quer converter? (metros,centimetros ou milhas)")
+if medida == "milhas" and convert == "metros":
  	result = valor*1609.344
- elif medida == "metros" and convert == "milhas":
+elif medida == "metros" and convert == "milhas":
  	result = valor/1609.344
- elif medida == "milhas" and convert == "centimetros":
+elif medida == "milhas" and convert == "centimetros":
  	result = valor*160934.4
- elif medida == "centimetros" and convert == "milhas":
+elif medida == "centimetros" and convert == "milhas":
  	result = valor/160934.4
- elif medida == "metros" and convert == "centimetros":
- 	result == valor*100
- elif medida == "centimetros" and convert == "metros":
- 	result == valor/100
- else:
+elif medida == "metros" and convert == "centimetros":
+ 	result = valor*100
+elif medida == "centimetros" and convert == "metros":
+ 	result = valor/100
+elif medida == convert:
+	result = valor
+else:
  	print("a unidade de medida está incorreta.")
- print(f"O resultado da conversão é : {result}")
+ 	result = 0
+print(f"O resultado da conversão é : {result}")
