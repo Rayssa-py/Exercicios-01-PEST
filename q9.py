39484913e0a88d935cb3cd2572fbf6534dc75e24
Rayssa Emelly Moura da Silva <rayssaemelly20@gmail.com>
2026-02-14 13:08:08 -0300

    Create q9.py

---
+++ q9.py
@@ -0,0 +1,7 @@
+#questão 9
+x1 = float(input(" digite o valor de x1: "))
+y1 = float(input(" digite o valor de y1: "))
+x2 = float(input(" digite o valor de x2: "))
+y2 = float(input(" digite o valor de y2: "))
+result = (((x2-x1)**2)+((y2-y1)**2))**(1/2)
+print(f"o valor da distancia de (x1,y1) e (x2,y2) é {result}")
